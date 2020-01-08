791717f1195b855f68d8ff6df2206ce3d55a8f0b
Anna Volkova-Kravchenko <anvolkravt@ya.ru>
2020-01-08 10:53:18 +0300

    add random quote functionality

---
+++ script.js
@@ -0,0 +1,22 @@
+const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random';
+const quoteDisplayElement = document.getElementById('quoteDisplay');
+const quoteInputElement = document.getElementById('quoteInput');
+
+function getRandomQuote() {
+  return fetch(RANDOM_QUOTE_API_URL)
+    .then(response => response.json())
+    .then(data => data.content);
+}
+
+async function renderNextQuote() {
+  const quote = await getRandomQuote();
+  quoteDisplayElement.innerHTML = quote;
+  quote.split('').forEach(character => {
+    const characterSpan = document.createElement('span');
+    characterSpan.innerText = character;
+    quoteDisplayElement.appendChild(characterSpan);
+  });
+  quoteInputElement.value = null;
+}
+
+renderNextQuote();
